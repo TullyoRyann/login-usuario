35e8da7845a2b39c93f1d1a39a8866bf7c075ff1
tullyoryann@gmail.com <Text147852>
2021-05-02 17:31:41 -0300

    Criação do projeto usuário

---
+++ sc_usuario/src/main/java/com/tullyo/scusuario/ScUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.tullyo.scusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScUsuarioApplication.class, args);
+	}
+
+}
---
+++ sc_usuario/src/test/java/com/tullyo/scusuario/ScUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tullyo.scusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
