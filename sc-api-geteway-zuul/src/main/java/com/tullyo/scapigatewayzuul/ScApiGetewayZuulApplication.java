a1ec5494b69a7c5cdebd9ef7661f1534650f91e6
tullyoryann@gmail.com <Text147852>
2021-05-06 10:33:30 -0300

    Adiciona projeto zuul

--- sc-api-geteway-zuul/src/main/java/com/tullyo/scapigatewayzuul/ScApiGetewayZuulApplication.java
+++ sc-api-geteway-zuul/src/main/java/com/tullyo/scapigatewayzuul/ScApiGetewayZuulApplication.java
@@ -1,15 +1,17 @@
-package com.tullyo.scusuario;
+package com.tullyo.scapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
-public class ScUsuarioApplication {
+public class ScApiGetewayZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScUsuarioApplication.class, args);
+		SpringApplication.run(ScApiGetewayZuulApplication.class, args);
 	}
 
 }
---
+++ sc-api-geteway-zuul/src/test/java/com/tullyo/scapigatewayzuul/ScApiGetewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tullyo.scapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScApiGetewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
