6857f794edd8d78d81152559e6a6b7587e7f3713
tullyoryann@gmail.com <Text147852>
2021-05-06 11:44:59 -0300

    Adiciona actuator e configuração do sc-usuario

--- sc-usuario/src/main/java/com/tullyo/scusuario/application/controller/UsuarioController.java
+++ sc-usuario/src/main/java/com/tullyo/scusuario/application/controller/UsuarioController.java
@@ -1,5 +1,9 @@
 package com.tullyo.scusuario.application.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,15 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tullyo.scusuario.domain.model.Usuario;
 import com.tullyo.scusuario.domain.service.UsuarioService;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioController {
 
 	private UsuarioService usuarioService;
+	
+	private Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
 
 	public UsuarioController(UsuarioService usuarioService) {
 		this.usuarioService = usuarioService;
 	}
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}	
 
 	@GetMapping(value = "/search")
 	public ResponseEntity<Usuario> search(@RequestParam String login) {
