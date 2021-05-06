31bfb64fa685ae4e62177090feed712d7db8c362
tullyoryann@gmail.com <Text147852>
2021-05-06 12:52:58 -0300

    projeto oauth criado

---
+++ sc-oauth/src/main/java/com/tullyo/scoauth/ScOauthApplication.java
@@ -0,0 +1,13 @@
+package com.tullyo.scoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScOauthApplication.class, args);
+	}
+
+}
